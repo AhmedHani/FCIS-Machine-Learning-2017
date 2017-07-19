5f8746945d6e3ac0ceba03258ed75c5370ae06fa
Ahmed Hani Ibrahim <ahani.fcis@gmail.com>
2017-07-20 01:34:07 +0200

    Bug fixes.

--- Session3/Practical/Solution/session3/softmax_regression.py
+++ Session3/Practical/Solution/session3/softmax_regression.py
@@ -72,13 +72,13 @@ print(len(iris_labels))
 iris_features, iris_labels = shuffle(iris_features, iris_labels)
 iris_labels = to_onehot(iris_labels)
 
-train_x, train_y, test_x, test_y = iris_features[0:89], iris_labels[0:89], iris_features[89:], iris_labels[89:]
+train_x, train_y, test_x, test_y = iris_features[0:139], iris_labels[0:139], iris_features[139:], iris_labels[139:]
 train_x, train_y, test_x, test_y = np.asarray(train_x), np.asarray(train_y), np.asarray(test_x), np.asarray(test_y)
 
 train_x, means, stds = standardize(train_x)
 test_x = standardize(test_x, means, stds)
 
-lr = SoftmaxRegression(learning_rate=0.02, epochs=800)
+lr = SoftmaxRegression(learning_rate=0.001, epochs=500)
 lr.fit(train_x, train_y)
 
 plt.plot(range(1, len(lr.cost_) + 1), np.log10(lr.cost_))
